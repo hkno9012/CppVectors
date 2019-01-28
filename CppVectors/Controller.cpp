42f40a8c1cee9686ed02c87c17c8477de9d3714b
HunterKnott <hunterknott00@gmail.com>
2019-01-28 13:54:22 -0700

    Finished assignment

--- CppVectors/Controller.cpp
+++ CppVectors/Controller.cpp
@@ -7,8 +7,33 @@
 //
 
 #include "Controller.hpp"
+#include <vector>
 
 void Controller :: start()
 {
-    cout << "Text stuff" << endl;
+    vector<int> numbers {1, 2, 3, 4, 5};
+    vector<string> shout {"fus", "ro", "dah"};
+    
+    for(int i = 0; i < 5; i++)
+    {
+        cout << numbers[i] << endl;
+    }
+    
+    cout << endl;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        cout << shout[i] << endl;
+    }
+    
+    cout << endl;
+    
+    string spam[3];
+    for(int j = 0; j < shout.size(); j++)
+    {
+        spam[j] = shout[j];
+        
+        cout << spam[j] << endl;
+    }
 }
+
